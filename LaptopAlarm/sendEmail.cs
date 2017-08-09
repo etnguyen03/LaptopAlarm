0dfa044d0fbff244cc6bc840b863eed964e36112
Ethan Nguyen <etnguyen03@hotmail.com>
2017-08-08 21:59:47 -0400

    SMTP message implemented. Might not work though

---
+++ LaptopAlarm/sendEmail.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace LaptopAlarm
+{
+    class sendEmail
+    {
+        private string myEmail, mySmtp, myUsername, myPassword;
+        private bool mySSL;
+
+        private static const string Subject = "LaptopAlarm - Alarm Triggered";
+        private static const string messageIntro = "Message from LaptopAlarm: Alarm triggered! Details: ";
+
+        public sendEmail(String email, String smtp, bool ssl, String username, String password)
+        {
+            myEmail = email;
+            mySmtp = smtp;
+            mySSL = ssl;
+            myUsername = username;
+            myPassword = password;
+        }
+        public void sendEmail(String alarmDescription = "NO DETAILS PROVIDED")
+        {
+            var fromAddress = new MailAddress(myEmail);
+            var toAddress = new MailAddress(myEmail);
+            int port;
+
+            if (mySSL)
+            {
+                port = 587;
+            }
+            else
+            {
+                port = 25;
+            }
+
+            var smtp = new SmtpClient();
+            if (myUsername == "" && myPassword == "")
+            {
+                smtp = new SmtpClient
+                {
+                    Host = mySmtp,
+                    Port = port,
+                    EnableSsl = mySSL,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false
+                };
+            }
+            else
+            {
+                smtp = new SmtpClient
+                {
+                    Host = mySmtp,
+                    Port = port,
+                    EnableSsl = mySSL,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(myUsername, myPassword)
+                };
+            }
+
+            using (var message = new MailMessage(fromAddress, toAddress){
+                Subject = Subject,
+                Body = messageIntro + alarmDescription
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
