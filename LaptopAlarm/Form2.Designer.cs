27630c15ec258c63c4d07872551f0205aeaf3479
Ethan Nguyen <etnguyen03@hotmail.com>
2017-10-23 18:26:11 -0400

    Change design of Form2

--- LaptopAlarm/Form2.Designer.cs
+++ LaptopAlarm/Form2.Designer.cs
@@ -52,19 +52,19 @@ namespace LaptopAlarm
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(125, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(215, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(277, 73);
+            this.label1.Size = new System.Drawing.Size(93, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "ALARM!";
+            this.label1.Text = "ALARM";
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 85);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 38);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(499, 252);
+            this.richTextBox1.Size = new System.Drawing.Size(499, 299);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
